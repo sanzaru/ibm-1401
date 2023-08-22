f54357da6ac838596b1ecc3fc0ebeede340db445
Martin Albrecht <sanzaru@users.noreply.github.com>
2023-08-23 00:34:38 +0200

    Initialize linesize correctly in 1402

--- Sources/IBM-1401-Emulator/IBM1402/IBM1402.swift
+++ Sources/IBM-1401-Emulator/IBM1402/IBM1402.swift
@@ -1,17 +1,21 @@
 import Foundation
 
 final class IBM1402 {
-    private var readStack = [[UInt8]]()
-    private var readIndex = 0
-
     enum LineSize: Int {
         case normal = 80, stub = 51
     }
 
-    private let linesize = 80 // Can be 51 on some models
+    private var readStack = [[UInt8]]()
+    private var readIndex = 0
+
+    private let linesize: LineSize
+
+    init(_ linesize: LineSize = .normal) {
+        self.linesize = linesize
+    }
 
     func load(from code: [UInt8]) -> [UInt8] {
-        readStack = code.chunked(into: 80).reversed()
+        readStack = code.chunked(into: linesize.rawValue).reversed()
 
         #if DEBUG
         dump("Loaded \(readStack.count) cards")
