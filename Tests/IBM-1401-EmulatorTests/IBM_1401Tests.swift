48e12e6845a185beab61adaaa32ac6fc2d5eb970
sanzaru <sanzaru@users.noreply.github.com>
2024-08-17 00:49:58 +0200

    Extended / fixed unit tests

--- Tests/IBM-1401-EmulatorTests/IBM_1401Tests.swift
+++ Tests/IBM-1401-EmulatorTests/IBM_1401Tests.swift
@@ -1,18 +1,10 @@
 import XCTest
 import class Foundation.Bundle
+import Lib1401
+
+@testable import IBM_1401_Emulator
 
 final class IBM_1401Tests: XCTestCase {
-    func testProcessExecution() throws {
-        let process = Process()
-        process.executableURL = productsDirectory.appendingPathComponent("IBM-1401-Emulator")
-
-        try process.run()
-        process.terminate()
-        process.waitUntilExit()
-
-        XCTAssertFalse(process.isRunning)
-    }
-
     /// Returns path to the built products directory.
     var productsDirectory: URL {
       #if os(macOS)
@@ -27,5 +19,40 @@ final class IBM_1401Tests: XCTestCase {
 
     static var allTests = [
         ("testProcessExecution", testProcessExecution),
+        ("testParityCheck", testParityCheck),
     ]
+}
+
+// MARK: General tests
+extension IBM_1401Tests {
+    func testProcessExecution() throws {
+        let process = Process()
+        process.executableURL = productsDirectory.appendingPathComponent("IBM-1401-Emulator")
+
+        try process.run()
+        process.terminate()
+        process.waitUntilExit()
+
+        XCTAssertFalse(process.isRunning)
+    }
+}
+
+
+// MARK: Word tests
+extension IBM_1401Tests {
+    func testParityCheck() throws {
+        Lib1401.CharacterEncodings.shared.simh.forEach { index, char in
+            XCTAssert(char.parityCheck, "Parity test failed for character \(index) with bit count: \(char.bitsSetCount)")
+
+            var wm: Word = Lib1401.CharacterEncodings.shared.simh["A"]!
+            wm.setWordMark()
+            XCTAssert(wm.parityCheck, "Parity test failed for character A (with WM) with bit count: \(wm.bitsSetCount)")
+        }
+    }
+
+    func testValidityCheck() throws {
+        Lib1401.CharacterEncodings.shared.simh.forEach { index, char in
+            XCTAssert(char.valid, "Validity test failed for character \(index): \(char.binaryString)")
+        }
+    }
 }
